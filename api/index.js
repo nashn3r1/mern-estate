d3ddda78d5eee8f854605107fb506d07a611c0a1
Ninad Deshpande <nashn3r1@gmail.com>
2024-01-11 20:29:18 +0530

    connected to db - 2

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+try {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    //dbName: "share_prompt",
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log("MongoDB connected");
+} catch (error) {
+  console.log(error);
+}
 
 const app = express();
 
