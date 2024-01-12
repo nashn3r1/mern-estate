43f4252d4dc4263e330030ae06a25a523bf25e55
Ninad Deshpande <nashn3r1@gmail.com>
2024-01-12 15:54:48 +0530

    added RTK

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { error, loading } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -19,7 +25,7 @@ function SignIn() {
     e.preventDefault();
     try {
       if (Object.keys(formData).length !== 0) {
-        setLoading(true);
+        dispatch(signInStart());
         const res = await fetch("/api/auth/signin", {
           method: "POST",
           headers: {
@@ -30,18 +36,15 @@ function SignIn() {
 
         const data = await res.json();
         if (data?.success === false) {
-          setError(data?.message);
-          setLoading(false);
+          dispatch(signInFailure(data?.message));
           return;
         }
 
-        setLoading(false);
-        setError(null);
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setLoading(false);
-      setError(error?.message);
+      dispatch(signInFailure(error?.message));
     }
   };
 
