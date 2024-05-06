4b6b6af5c23a8bdf46fb1b12070927820545040a
Ninad Deshpande <nashn3r1@gmail.com>
2024-05-06 16:57:33 +0530

    completed home page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -114,7 +114,7 @@ function CreateListing() {
       if (data.success === false) {
         setError(data.message);
       }
-      navigate(`/${data._id}`);
+      navigate(`/listing/${data._id}`);
     } catch (error) {
       setError(error.message);
       setLoading(false);
